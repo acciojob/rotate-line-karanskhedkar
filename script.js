61fef3b6bccb30eedbe4aef2d65a28c119568979
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-19 12:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let angle = 0;
+
+const rotate = () => {
+  const line = document.getElementById("line");
+  console.log(line)
+  if (line) {
+    setInterval(() => {
+      angle = angle + 2;
+      line.style.transform += `rotate(${angle}deg)`;
+    }, 2000);
+  }
+};
+rotate();
